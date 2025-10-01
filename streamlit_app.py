e59738d2e31a8aa9ee64e618003c8a0e2561fe72
taelin-668 <taelin668@gmail.com>
2025-10-01 09:10:46 +0000

    첫번째 앱 배포

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈송태린의 첫 번째 앱!")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+    "망럽단 모집합니다")
+st.info("망고빙수를 좋아해요")
+st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFzAdeEowTtiKMJe_5tOepfi366f7xyk6mjQ&s")
